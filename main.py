687724f04da1b7d812d95773652bbcd960a5cdb0
Sergey Mezentsev <thebits@yandex.ru>
2015-11-14 18:47:45 +0300

    working speech

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import uuid
 UUID = uuid.uuid1()
 
 MIC_TIMEOUT = 5
-OUT_FILE = 'out.wav'
+OUT_FILE = 'speech-mmpc.mp3'
 API_KEY = 'a4842bb7-929b-4d4d-9982-4a1eff599563'
 
 def save_mic(mic_timeout=MIC_TIMEOUT, out_file=OUT_FILE):
-    subprocess.call(["timeout", str(mic_timeout), 'ffmpeg',  '-y', '-ar', '16000', '-acodec', 'pcm_s16be', '-f',  'alsa', '-i', 'pulse', out_file])
+    subprocess.call(["timeout", str(mic_timeout), 'ffmpeg',  '-y', '-f',  'alsa', '-i', 'pulse', '-acodec', 'libmp3lame', out_file])
 
 def recognize(audio_file):
     url =   'https://asr.yandex.net/asr_xml?'+\
     'uuid=' + str(UUID.hex) + \
     '&key=' + API_KEY + \
-    '&topic=numbers' + \
+    '&topic=queries' + \
     '&lang=ru-RU'
-    command = ['curl', '-X', 'POST', '--data-binary', '@'+OUT_FILE, url, '-H', "Content-Type: audio/x-wav"]
-    command = ['curl', '-v', '-X', 'POST', '--data-binary', '@'+OUT_FILE, url, '-H', "Content-Type: audio/x-pcm;bit=16;rate=16000"]
+    command = ['curl', '-v', '-s', '-X', 'POST', '--data-binary', '@'+OUT_FILE, url, '-H', "Content-Type: audio/x-mpeg-3"]
     print('command: ' + ' '.join(command))
     out_text = subprocess.check_output(command)
     out_text = str(out_text)
